f23a7efcbfc6afa417b6bd7f16bd15aaed9aa419
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-09-16 14:16:58 +0100

    fix logger

--- logger.go
+++ logger.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func handlerLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write(fmt.Appendf(nil, "request: %s %s", r.Method, r.URL.Path))
+		log.Printf("request: %s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
