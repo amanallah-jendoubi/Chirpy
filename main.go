c6402cbd200fe60965fd05a4249a98cd6b312093
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-09-16 12:46:18 +0100

    add server hits counter

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "./public"
 	const port = "8080"
 
+	cfg := &apiConfig{}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.Handle("/time", timeHandler(time.RFC1123))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz/", handlerReadiness)
+	mux.Handle("/metrics/", cfg.handlerMetrics())
+	mux.Handle("/reset/", cfg.handlerReset())
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -25,13 +33,7 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func timeHandler(format string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("the time is: " + time.Now().Format(format)))
-	})
-}
+//handlers
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -39,9 +41,32 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handlerMetrics() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(fmt.Appendf(nil, "Hits: %d", cfg.fileserverHits.Load()))
+	}
+}
+
+func (cfg *apiConfig) handlerReset() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+	}
+}
+
+//middlewares
+
 func middlewareLog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
