3de738a39e15231c9506bb5c7281826338df7669
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-09-16 12:02:09 +0100

    add time handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -10,20 +11,37 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(filepathRoot))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/time", timeHandler(time.RFC1123))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: mux,
+		Handler: middlewareLog(mux),
 	}
 
 	log.Printf("Running on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
 
+func timeHandler(format string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("the time is: " + time.Now().Format(format)))
+	})
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("we good to go !"))
+	w.Write([]byte("OK"))
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
