46a3ec36a984b16484e0fdeb4fadcb8d09cd7b8c
amanallah-jendoubi <amanallah.jendoubi@gmail.com>
2026-09-16 19:26:48 +0100

    add cleanBody validation to chirp request

--- validate-chirp.go
+++ validate-chirp.go
@@ -4,14 +4,15 @@ import (
 	"encoding/json"
 	"github.com/amanallah-jendoubi/Textio/helpers"
 	"net/http"
+	"strings"
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	type chirpRequest struct {
 		Body string `json:"body"`
 	}
-	type validResponse struct {
-		Valid bool `json:"valid"`
+	type response struct {
+		CleanedBody string `json:"cleaned_body"`
 	}
 	dec := json.NewDecoder(r.Body)
 	var params chirpRequest
@@ -24,5 +25,16 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		helpers.RespondWithError(w, 400, "Chirp is too long !")
 		return
 	}
-	helpers.RespondWithJSON(w, 200, validResponse{Valid: true})
+	helpers.RespondWithJSON(w, 200, response{CleanedBody: cleanBody(params.Body)})
+}
+
+func cleanBody(body string) (cleanedBody string) {
+	s := strings.ToLower(body)
+	words := strings.Split(s, " ")
+	for i, w := range words {
+		if w == "kerfuffle" || w == "sharbert" || w == "fornax" {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
 }
